4368893e5b888e16a56acf4b4fca42ffda33439d
Sukrit <roysukrit78@gmail.com>
2024-02-16 18:33:22 +0530

    Adde CORS origin

--- server/server.js
+++ server/server.js
@@ -237,7 +237,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors({
+    origin:process.env.CLIENT_URL
+}))
 
 app.use('/api', apiRouter);
 
